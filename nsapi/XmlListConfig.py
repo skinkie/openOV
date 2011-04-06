be2a63c02a2c75ac265951c01b9181848372ac7e
Stefan de Konink <stefan@konink.de>
2011-04-06 16:29:39 +0200

    NSAPI componenten van https://nsapi.openov.nl/

---
+++ nsapi/XmlListConfig.py
@@ -0,0 +1,16 @@
+import cElementTree as ElementTree
+
+class XmlListConfig(list):
+    def __init__(self, aList):
+        for element in aList:
+            if element:
+                # treat like dict
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    self.append(XmlDictConfig(element))
+                # treat like list
+                elif element[0].tag == element[1].tag:
+                    self.append(XmlListConfig(element))
+            elif element.text:
+                text = element.text.strip()
+                if text:
+                    self.append(text)
---
+++ nsapi/nsapi.py
@@ -0,0 +1,52 @@
+import web
+import base64
+import re
+import httplib2
+import cElementTree as ElementTree 
+from XmlDictConfig import XmlDictConfig
+import simplejson 
+
+urls = (
+    '/xml/(.*)', 'NSAPI',
+    '/json/(.*)','ToJSON'
+)
+
+app = web.application(urls,globals())
+
+class NSAPI:
+    def processor(self, content):
+    	return content
+
+    def GET(self, request):
+        auth = web.ctx.env.get('HTTP_AUTHORIZATION')
+        authreq = False
+        if auth is None:
+            authreq = True
+        else:
+            auth = re.sub('^Basic ','',auth)
+            username,password = base64.decodestring(auth).split(':')
+
+	    h = httplib2.Http()
+	    h.debuglevel = 1
+	    h.add_credentials(username, password)
+	    resp, content = h.request("https://webservices.ns.nl/"+request)
+
+            if resp['status'] == '200':
+	    	return self.processor(content)
+	    if resp['status'] == '404':
+	        return '404 Not Found'
+            else:
+                authreq = True
+        if authreq:
+            web.header('WWW-Authenticate','Basic realm="openOV-NS"')
+            web.ctx.status = '401 Unauthorized'
+            return 'Gelieve een geldig NS API account te gebruiken.'+request
+
+class ToJSON(NSAPI):
+    def processor(self, content):
+        root = ElementTree.XML(content)
+        xmldict = XmlDictConfig(root)
+        web.header('Content-Type', 'application/json')
+        return simplejson.dumps(xmldict)
+ 
+application = app.wsgifunc()
